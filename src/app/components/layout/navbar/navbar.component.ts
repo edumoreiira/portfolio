71ad0bf964b8629a0b5f8dd878b83c25aa111e3b
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-05-07 18:55:59 -0300

    feat: create navbar

--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -1,9 +1,24 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: 'navbar',
+    selector: 'app-navbar',
     imports: [],
-    template: `<h1>navbar</h1>`,
+    host: {
+        class: 'flex items-center justify-between py-3 px-4 sm:px-8 lg:px-12 gap-6 backdrop-blur bg-neutral-950/50 sticky top-0 w-full z-20'
+    },
+    template: `
+    <span class="text-xl font-medium font-[Kanit]">[edumoreira]</span>
+    <nav>
+        <ul class="flex items-center gap-4">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Trabalhos</a></li>
+            <li><a href="#">Sobre</a></li>
+        </ul>
+    </nav>
+    <button class="px-4 py-2 rounded-xl border border-neutral-700 min-w-fit">
+        Entrar em contato
+    </button>
+    `
 })
 export class NavbarComponent {
 
