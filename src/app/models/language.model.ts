4f22905b4f11867aa97f45d94416d8972ea7f214
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-05-09 15:37:14 -0300

    feat: add main translation

--- src/app/models/language.model.ts
+++ src/app/models/language.model.ts
@@ -6,17 +6,31 @@ export interface LanguageApllication {
             about: string;
         }
         contact: string;
+    },
+    main: {
+        misc: string;
+        title: string[];
+        description: string;
+        button1: string;
+        button2: string;
     }
 }
 
 export const language_pt_br: LanguageApllication = {
     navbar: {
         menu: {
-            home: "Início",
+            home: "Home",
             works: "Trabalhos",
             about: "Sobre",
         },
         contact: "Entrar em contato",
+    },
+    main: {
+        misc: "Desenvolvedor Angular",
+        title: ["Olá, me chamo", "Eduardo", "Sou", "Desenvolvedor", "e", "Designer" ],
+        description: "Desenvolvo sites modernos, rápidos e com design de impacto. Transformo ideias em páginas atraentes, funcionais e feitas para valorizar seu negócio. Se você precisa de uma landing page profissional, posso te ajudar a destacar sua marca na internet.",
+        button1: "Ver trabalhos",
+        button2: "Sobre Mim",
     }
 }
 
@@ -28,5 +42,12 @@ export const language_en_us: LanguageApllication = {
             about: "About",
         },
         contact: "Contact me",
+    },
+    main: {
+        misc: "Angular Developer",
+        title: ["Hello, my name is", "Eduardo", "I'm", "Developer", "and", "Designer"],
+        description: "I build modern, fast websites with clean, impactful design. I turn ideas into attractive, functional pages crafted to highlight and grow your business. If you need a professional landing page, I can help your brand stand out online with confidence.",
+        button1: "See works",
+        button2: "About me",
     }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LANGUAGE_APPLICATION } from '../../tokens/language.tokens';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  protected lg = inject(LANGUAGE_APPLICATION);
 }
