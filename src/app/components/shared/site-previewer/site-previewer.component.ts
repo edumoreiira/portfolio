141d8c9c5e70178afdfc63aac1fc795d76e2b470
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-05-10 18:47:59 -0300

    feat: create initial structure for site preview component

---
+++ src/app/components/shared/site-previewer/site-previewer.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-site-previewer',
+  host: {
+    class: 'border border-neutral-800 rounded-xl overflow-hidden'
+  },
+  imports: [],
+  templateUrl: './site-previewer.component.html',
+  styleUrl: './site-previewer.component.scss'
+})
+export class SitePreviewerComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { LANGUAGE_APPLICATION } from '../../tokens/language.tokens';
 import { ButtonComponent } from '../../components/base/button.component';
+import { SitePreviewerComponent } from "../../components/shared/site-previewer/site-previewer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, SitePreviewerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
