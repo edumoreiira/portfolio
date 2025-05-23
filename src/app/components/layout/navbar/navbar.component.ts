d812f1ef280227ebcdf726ee49688c84bcd0a17f
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-05-23 13:12:54 -0300

    feat: add meta tags

--- src/app/components/layout/navbar/navbar.component.ts
+++ src/app/components/layout/navbar/navbar.component.ts
@@ -22,16 +22,20 @@ import { NgClass } from "@angular/common";
         <nav class="sm:static absolute max-w-[calc(100%-1.5rem)] right-0 top-full sm:py-0 sm:px-0 py-6 px-8 sm:bg-transparent bg-neutral-950/95 
         sm:rounded-none rounded-bl-2xl sm:border-none border-l border-b border-neutral-700/50 overflow-hidden z-10" @slideNavbar>
             <ul class="flex sm:items-center sm:gap-8 gap-6 flex-col sm:flex-row text-neutral-200 font-semibold">
-                <li><a class="cursor-pointer sm:p-0 p-1 hover:text-white transition-colors" tabIndex="0"
+
+                <li><button [attr.aria-label]="'Go to ' + nav().menu.home" class="cursor-pointer sm:p-0 p-1 hover:text-white transition-colors" tabIndex="0"
                 (click)="scrollTo('home')"
-                (keydown)="onAnchorKeydown($event, 'home')"> {{ nav().menu.home }} </a></li>
-                <li><a class="cursor-pointer sm:p-0 p-1 hover:text-white transition-colors" tabIndex="0"
+                (keydown)="onAnchorKeydown($event, 'home')"> {{ nav().menu.home }} </button></li>
+
+                <li><button [attr.aria-label]="'Go to ' + nav().menu.works" class="cursor-pointer sm:p-0 p-1 hover:text-white transition-colors" tabIndex="0"
                 (click)="scrollTo('my-works')"
-                (keydown)="onAnchorKeydown($event, 'my-works')"> {{ nav().menu.works }} </a></li>
-                <li><a class="cursor-pointer sm:p-0 p-1 hover:text-white transition-colors" tabIndex="0"
+                (keydown)="onAnchorKeydown($event, 'my-works')"> {{ nav().menu.works }} </button></li>
+
+                <li><button [attr.aria-label]="'Go to ' + nav().menu.techs" class="cursor-pointer sm:p-0 p-1 hover:text-white transition-colors" tabIndex="0"
                 (click)="scrollTo('techs')"
-                (keydown)="onAnchorKeydown($event, 'techs')"> {{ nav().menu.techs }} </a></li>
+                (keydown)="onAnchorKeydown($event, 'techs')"> {{ nav().menu.techs }} </button></li>
                 <li>
+
                 <button custom-btn variant="outline"
                 class="px-4 py-2 rounded-xl sm:hidden block bg-neutral-950"
                 (click)="scrollTo('contact')">
@@ -49,6 +53,7 @@ import { NgClass } from "@angular/common";
         class="text-[.95rem]"
         (clickedItem)="changeLanguage($event.value)"
         >
+            <span class="sr-only">Change language</span>
             <i class="fi fi-rr-language-exchange flex text-3xl sm:text-2xl hover:text-neutral-300 transition-colors"></i>
         </dropdown-selection>
         <button custom-btn variant="outline" class="px-4 py-2 hidden sm:block"
